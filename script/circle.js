7498b526b02f2a2b58b11036af99588feb133bc4
thomas <thomaslolkschmidt@gmail.com>
2019-12-13 19:57:19 +0100

    Initial commit.

---
+++ script/circle.js
@@ -0,0 +1,10 @@
+function radius2diameter(radius) {
+    var diameter = 2*radius;
+    return diameter;
+}
+
+
+function radius2circumference(radius) {
+    var circumference = 2*pi*radius;
+    return circumference;
+}
