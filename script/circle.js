bb46c160532c2e4e10c8a5cd5359e9f80ab5a0d0
thomas <thomaslolkschmidt@gmail.com>
2019-12-13 21:36:17 +0100

    Added function to write calculation output on webpage.

--- script/circle.js
+++ script/circle.js
@@ -5,6 +5,15 @@ function radius2diameter(radius) {
 
 
 function radius2circumference(radius) {
-    var circumference = 2*pi*radius;
+    var circumference = 2*Math.PI*radius;
     return circumference;
+}
+
+
+function calculate_circle_parameters() {
+    var radius = document.getElementById("radius").value;
+    var diameter = radius2diameter(radius);
+    var circumference = radius2circumference(radius);
+    document.getElementById("diameter").innerHTML = diameter;
+    document.getElementById("circumference").innerHTML = circumference;
 }
